07c39f1080f7c9e31c4fc9ec72cf9d5187c8cd49
Jeff Gore <jgore00@gmail.com>
2021-05-26 07:37:14 -0400

    test(browser): various improvements to homepage test
    I moved away from wanting *every* test to be run in both desktop and mobile view. Now the only conditional viewport tests are for the aspects that are actually different based on the viewport size, which is really just links vs. hamburger menu in the header.
    
    In the future, the totality of the app on a small screen size would be properly tested by using Appium or something to test in a true mobile device setting.

--- test-browser/specs/homepage.browser.test.ts
+++ test-browser/specs/homepage.browser.test.ts
@@ -1,60 +1,111 @@
 import homepage from '../pages/home.page';
+import { footerLinks } from '../../src/client/components/footer';
+
+const viewportScenarios = {
+  WIDE: 'the viewport width is greater than or equal to 700px',
+  NARROW: 'the viewport width is less than 700px',
+};
 
 describe('The homepage', () => {
   beforeAll(() => {
     homepage.open();
   });
 
-  ['desktop', 'mobile'].forEach((screenSize) => {
-    describe(`On ${screenSize}`, () => {
-      beforeAll(() => {
-        if (screenSize === 'mobile') {
-          // Simulate mobile window (Pixel 2)
-          browser.setWindowSize(411, 731);
-        }
-      });
-      it('should display a heading', () => {
-        expect(homepage.heading.isDisplayed()).toBe(true);
-      });
-      describe('Footer', () => {
-        /* These icons are also links that open in a new tab, but this aspect
-        is already handled by unit tests and turned out to be unreliable in browser
-        tests due to varying behavior when a link is accessed. */
-        it('should display a GitHub icon', () => {
-          expect(homepage.githubFooterIcon.isDisplayed()).toEqual(true);
-        });
-        it('should display a LinkedIn icon', () => {
-          expect(homepage.linkedinFooterIcon.isDisplayed()).toEqual(true);
-        });
-        it('should display a Twitter icon', () => {
-          expect(homepage.twitterFooterIcon.isDisplayed()).toEqual(true);
-        });
-        it('should display a Instagram icon', () => {
-          expect(homepage.instagramFooterIcon.isDisplayed()).toEqual(true);
-        });
-      });
-
-      if (screenSize === 'desktop') {
-        it('should display an "About Me" link', () => {
-          expect(homepage.aboutMeLink.isDisplayed()).toBe(true);
-        });
-        it('should display a "Projects" link', () => {
-          expect(homepage.projectsLink.isDisplayed()).toBe(true);
-        });
-        it('should display a "Things I Like" link', () => {
-          expect(homepage.thingsILikeLink.isDisplayed()).toBe(true);
-        });
-      }
-      if (screenSize === 'mobile') {
-        it('should not display the "About Me", "Projects", or "Things I Like" links', () => {
-          expect(homepage.aboutMeLink.isDisplayed()).toBe(false);
-          expect(homepage.projectsLink.isDisplayed()).toBe(false);
-          expect(homepage.thingsILikeLink.isDisplayed()).toBe(false);
-        });
-        it('should display a hamburger icon', () => {
-          expect(homepage.mobileHamburgerMenuIcon.isDisplayed()).toBe(true);
-        });
-      }
+  describe('Footer', () => {
+    /* Originally the links were tested by actually clicking them and checking that the new tab
+    URL matched the link, but the behavior of other websites cannot be controlled. For instance sometimes
+    the LinkedIn page would redirect to a different page. */
+    it('should display a GitHub icon which is a link to my Github page', () => {
+      expect(homepage.githubFooterIcon.isDisplayed()).toEqual(true);
+      expect(homepage.githubFooterIcon.getAttribute('href')).toEqual(
+        footerLinks.GITHUB_URL,
+      );
     });
+    it('should display a LinkedIn icon which is a link to my LinkedIn page', () => {
+      expect(homepage.linkedinFooterIcon.isDisplayed()).toEqual(true);
+      expect(homepage.linkedinFooterIcon.getAttribute('href')).toEqual(
+        footerLinks.LINKEDIN_URL,
+      );
+    });
+    it('should display a Twitter icon which is a link to my Twitter page', () => {
+      expect(homepage.twitterFooterIcon.isDisplayed()).toEqual(true);
+      expect(homepage.twitterFooterIcon.getAttribute('href')).toEqual(
+        footerLinks.TWITTER_URL,
+      );
+    });
+    it('should display a Instagram icon which is a link to my Instagram page', () => {
+      expect(homepage.instagramFooterIcon.isDisplayed()).toEqual(true);
+      expect(homepage.instagramFooterIcon.getAttribute('href')).toEqual(
+        footerLinks.INSTAGRAM_URL,
+      );
+    });
+  });
+
+  describe('Header', () => {
+    it('should display my name as the main <h1> header', () => {
+      expect(homepage.heading.getText()).toEqual('Jeff Gore');
+    });
+
+    /* TODO: use wdio-image-comparison-service for visual regression testing */
+    it('should display my name in a custom font', () => {
+      expect(homepage.heading.getCSSProperty('font-family').value).toEqual(
+        'jetbrainsmono',
+      );
+    });
+
+    /* TODO: Once you have devices tested as part of WDIO capabilities, then it would look more like
+    supportedViewportScenarios.forEach */
+    [viewportScenarios.WIDE, viewportScenarios.NARROW].forEach(
+      (viewportScenario) => {
+        describe(`When ${viewportScenario}`, () => {
+          let originalWindowSize: { width: number; height: number };
+
+          beforeAll(() => {
+            if (viewportScenario === viewportScenarios.NARROW) {
+              /* TODO: only call setWindowSize if we know it's a non-mobile device. Resizing a 
+              window isn't feasible or necessary on a mobile device. */
+              originalWindowSize = browser.getWindowSize();
+              browser.setWindowSize(411, 731);
+            }
+          });
+
+          afterAll(() => {
+            if (viewportScenario === viewportScenarios.NARROW) {
+              const { width, height } = originalWindowSize;
+              browser.setWindowSize(width, height);
+            }
+          });
+
+          if (viewportScenario === viewportScenarios.WIDE) {
+            /* TODO: check with server first what links *should* be appearing. If a link is 
+            disabled, expect that it does **not** appear. */
+            it('should display an "About Me" link', () => {
+              expect(homepage.aboutMeLink.isDisplayed()).toBe(true);
+            });
+            it('should display a "Projects" link', () => {
+              expect(homepage.projectsLink.isDisplayed()).toBe(true);
+            });
+            it('should display a "Things I Like" link', () => {
+              expect(homepage.thingsILikeLink.isDisplayed()).toBe(true);
+            });
+            it('should not display a hamburger icon', () => {
+              expect(homepage.mobileHamburgerMenuIcon.isDisplayed()).toBe(
+                false,
+              );
+            });
+          }
+          if (viewportScenario === viewportScenarios.NARROW) {
+            it('should not display the "About Me", "Projects", or "Things I Like" links', () => {
+              expect(homepage.aboutMeLink.isDisplayed()).toBe(false);
+              expect(homepage.projectsLink.isDisplayed()).toBe(false);
+              expect(homepage.thingsILikeLink.isDisplayed()).toBe(false);
+            });
+            it('should display a hamburger icon', () => {
+              expect(homepage.mobileHamburgerMenuIcon.isDisplayed()).toBe(true);
+            });
+          }
+        });
+      },
+    );
   });
 });
