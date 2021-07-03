ee0eb1f538803a3a0518b8ccb44b4493fb5e6e87
Jeff Gore <jgore00@gmail.com>
2021-07-03 13:56:10 -0400

    chore: enable code splitting and split mobile drawer component

--- src/client/components/navigator/mobile-menu/index.tsx
+++ src/client/components/navigator/mobile-menu/index.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
-import Drawer from '@material-ui/core/SwipeableDrawer';
+import loadable from '@loadable/component';
 
 import { StyledButton, StyledLink, LinkPanel } from './styled-components';
 import { navMenuEnabledLinks } from '../../../../shared/constants';
 
 export function MobileNavMenu(): React.ReactElement {
+  const DrawerModule = loadable.lib(
+    () => import('@material-ui/core/SwipeableDrawer'),
+  );
+
   const [drawerOpen, setDrawerOpen] = useState(false);
 
   const openDrawer = () => setDrawerOpen(true);
@@ -15,21 +19,25 @@ export function MobileNavMenu(): React.ReactElement {
       <StyledButton type="button" onClick={openDrawer}>
         ≡
       </StyledButton>
-      <Drawer
-        anchor="right"
-        open={drawerOpen}
-        onOpen={openDrawer}
-        onClose={closeDrawer}
-        classes={{
-          paperAnchorRight: 'jg-drawer-open', // This says to apply this custom class when the "paper" (the actual visible white "drawer") is anchored right, which it is (see other props).
-        }}
-      >
-        {Array.from(navMenuEnabledLinks).map(([title, path]) => (
-          <StyledLink to={path} key={title} onClick={closeDrawer}>
-            <LinkPanel>{title}</LinkPanel>
-          </StyledLink>
-        ))}
-      </Drawer>
+      <DrawerModule>
+        {({ default: Drawer }) => (
+          <Drawer
+            anchor="right"
+            open={drawerOpen}
+            onOpen={openDrawer}
+            onClose={closeDrawer}
+            classes={{
+              paperAnchorRight: 'jg-drawer-open', // This says to apply this custom class when the "paper" (the actual visible white "drawer") is anchored right, which it is (see other props).
+            }}
+          >
+            {Array.from(navMenuEnabledLinks).map(([title, path]) => (
+              <StyledLink to={path} key={title} onClick={closeDrawer}>
+                <LinkPanel>{title}</LinkPanel>
+              </StyledLink>
+            ))}
+          </Drawer>
+        )}
+      </DrawerModule>
     </div>
   );
 }
