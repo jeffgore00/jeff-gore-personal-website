88b311533cbdfad202052e8df2fddf2060d458b2
Jeff Gore <jgore00@gmail.com>
2023-07-24 21:34:16 -0400

    test(browser): always retry 1st test in case of wdio ECONNRESET
    This bug is very persistent and I can't see any solution online or in the stack trace. It always affects just the first test and it may be a race condition between the test runner and chromedriver.

--- test-browser/specs/blog.browser.test.ts
+++ test-browser/specs/blog.browser.test.ts
@@ -10,70 +10,85 @@ describe('The Blogs page', () => {
 
   describe('When the content has loaded', () => {
     it('Should show previews for all blogs, from newest to oldest', async () => {
-      const blogPreviews = await blogPage.getStructuredBlogPreviews();
-      expect(blogPreviews.length).toEqual(8);
+      let testComplete = false;
+      while (!testComplete) {
+        try {
+          // eslint-disable-next-line no-await-in-loop
+          const blogPreviews = await blogPage.getStructuredBlogPreviews();
+          expect(blogPreviews.length).toEqual(8);
 
-      expect(blogPreviews[0].type).toEqual('TECH');
-      expect(blogPreviews[0].title).toEqual(
-        'The Algorithms That Still Matter (4/2/2050)',
-      );
-      expect(blogPreviews[0].subtitle).toEqual(
-        'A cheat sheet to some fundamentals that are older than me.',
-      );
+          expect(blogPreviews[0].type).toEqual('TECH');
+          expect(blogPreviews[0].title).toEqual(
+            'The Algorithms That Still Matter (4/2/2050)',
+          );
+          expect(blogPreviews[0].subtitle).toEqual(
+            'A cheat sheet to some fundamentals that are older than me.',
+          );
 
-      expect(blogPreviews[1].type).toEqual('TECH');
-      expect(blogPreviews[1].title).toEqual(
-        'WebAssembly - A Blast From The Past (4/1/2050)',
-      );
-      expect(blogPreviews[1].subtitle).toEqual(
-        'A look back at the language that was the hotness during the 2030s.',
-      );
+          expect(blogPreviews[1].type).toEqual('TECH');
+          expect(blogPreviews[1].title).toEqual(
+            'WebAssembly - A Blast From The Past (4/1/2050)',
+          );
+          expect(blogPreviews[1].subtitle).toEqual(
+            'A look back at the language that was the hotness during the 2030s.',
+          );
 
-      expect(blogPreviews[2].type).toEqual('COMMENTARY');
-      expect(blogPreviews[2].title).toEqual(
-        'Good Design Is A Human Right (3/15/2050)',
-      );
-      expect(blogPreviews[2].subtitle).toEqual(
-        "We shouldn't have to look at ugly things. Inside, a proposal to codify that into international law.",
-      );
+          expect(blogPreviews[2].type).toEqual('COMMENTARY');
+          expect(blogPreviews[2].title).toEqual(
+            'Good Design Is A Human Right (3/15/2050)',
+          );
+          expect(blogPreviews[2].subtitle).toEqual(
+            "We shouldn't have to look at ugly things. Inside, a proposal to codify that into international law.",
+          );
 
-      expect(blogPreviews[3].type).toEqual('COMMENTARY');
-      expect(blogPreviews[3].title).toEqual(
-        'Remember When We Had to Remember? (3/1/2050)',
-      );
-      expect(blogPreviews[3].subtitle).toEqual(
-        'With the latest Neuralink, we welcome the end of human memory as we know it. Is that a good thing?',
-      );
+          expect(blogPreviews[3].type).toEqual('COMMENTARY');
+          expect(blogPreviews[3].title).toEqual(
+            'Remember When We Had to Remember? (3/1/2050)',
+          );
+          expect(blogPreviews[3].subtitle).toEqual(
+            'With the latest Neuralink, we welcome the end of human memory as we know it. Is that a good thing?',
+          );
 
-      expect(blogPreviews[4].type).toEqual('TECH');
-      expect(blogPreviews[4].title).toEqual(
-        'Coding Your Printed-Flesh Friends With DNAScript (2/2/2050)',
-      );
-      expect(blogPreviews[4].subtitle).toEqual(
-        'Template-farmed humans are for casuals. As always, tinkering with the source yields the best results.',
-      );
+          expect(blogPreviews[4].type).toEqual('TECH');
+          expect(blogPreviews[4].title).toEqual(
+            'Coding Your Printed-Flesh Friends With DNAScript (2/2/2050)',
+          );
+          expect(blogPreviews[4].subtitle).toEqual(
+            'Template-farmed humans are for casuals. As always, tinkering with the source yields the best results.',
+          );
 
-      expect(blogPreviews[5].type).toEqual('COMMENTARY');
-      expect(blogPreviews[5].title).toEqual('My Trip To Mars (2/1/2050)');
-      expect(blogPreviews[5].subtitle).toEqual(
-        "A fun extraterrestrial jaunt, but oddly, everything smells like Cap'n Crunch.",
-      );
+          expect(blogPreviews[5].type).toEqual('COMMENTARY');
+          expect(blogPreviews[5].title).toEqual('My Trip To Mars (2/1/2050)');
+          expect(blogPreviews[5].subtitle).toEqual(
+            "A fun extraterrestrial jaunt, but oddly, everything smells like Cap'n Crunch.",
+          );
 
-      expect(blogPreviews[6].type).toEqual('TECH');
-      expect(blogPreviews[6].title).toEqual(
-        'Testing Open Source Holographs (1/2/2050)',
-      );
-      expect(blogPreviews[6].subtitle).toEqual(
-        "Yes, it's possible. Here's how to plork this daunting task.",
-      );
+          expect(blogPreviews[6].type).toEqual('TECH');
+          expect(blogPreviews[6].title).toEqual(
+            'Testing Open Source Holographs (1/2/2050)',
+          );
+          expect(blogPreviews[6].subtitle).toEqual(
+            "Yes, it's possible. Here's how to plork this daunting task.",
+          );
 
-      expect(blogPreviews[7].type).toEqual('COMMENTARY');
-      expect(blogPreviews[7].title).toEqual(
-        "Holy God, I'm 64 Years Old (1/1/2050)",
-      );
-      expect(blogPreviews[7].subtitle).toEqual(
-        'A reflection on decades of making fart noises and bird calls.',
-      );
+          expect(blogPreviews[7].type).toEqual('COMMENTARY');
+          expect(blogPreviews[7].title).toEqual(
+            "Holy God, I'm 64 Years Old (1/1/2050)",
+          );
+          expect(blogPreviews[7].subtitle).toEqual(
+            'A reflection on decades of making fart noises and bird calls.',
+          );
+          testComplete = true;
+        } catch (err) {
+          if (String(err).includes('ECONNRESET')) {
+            // eslint-disable-next-line no-console
+            console.log('Connection error, retrying');
+          } else {
+            testComplete = true;
+            throw err;
+          }
+        }
+      }
     });
 
     it('should display "Blog" in the <title>', async () => {
